97140f744d68cb16eebf9bf005ac81fb1669fda8
Snownee <i@snownee.com>
2024-03-27 00:57:38 +0800

    feat: game object gen, part 3, nuke builtin tabs

--- src/main/java/org/teacon/xkdeco/util/CommonProxy.java
+++ src/main/java/org/teacon/xkdeco/util/CommonProxy.java
@@ -1,7 +1,11 @@
 package org.teacon.xkdeco.util;
 
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Map;
 import java.util.Objects;
+import java.util.function.Consumer;
 
 import org.teacon.xkdeco.XKDeco;
 import org.teacon.xkdeco.block.behavior.BlockBehaviorRegistry;
@@ -18,11 +22,23 @@ import org.teacon.xkdeco.init.XKDecoObjects;
 import javax.annotation.ParametersAreNonnullByDefault;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.SharedConstants;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.metadata.pack.PackMetadataSection;
+import net.minecraft.server.packs.repository.FolderRepositorySource;
+import net.minecraft.server.packs.repository.Pack;
+import net.minecraft.server.packs.repository.PackRepository;
+import net.minecraft.server.packs.repository.PackSource;
+import net.minecraft.server.packs.repository.RepositorySource;
+import net.minecraft.server.packs.resources.MultiPackResourceManager;
+import net.minecraft.server.packs.resources.ReloadableResourceManager;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.CreativeModeTab;
@@ -30,18 +46,28 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.common.CreativeModeTabRegistry;
+import net.minecraftforge.client.loading.ClientModLoader;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.fml.Logging;
+import net.minecraftforge.fml.ModLoader;
+import net.minecraftforge.fml.ModLoadingStage;
+import net.minecraftforge.fml.ModLoadingWarning;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.fml.loading.FMLPaths;
+import net.minecraftforge.forgespi.language.IModInfo;
+import net.minecraftforge.forgespi.locating.IModFile;
 import net.minecraftforge.registries.NewRegistryEvent;
 import net.minecraftforge.registries.RegisterEvent;
 import net.minecraftforge.registries.RegistryBuilder;
+import net.minecraftforge.resource.DelegatingPackResources;
+import net.minecraftforge.resource.PathPackResources;
+import net.minecraftforge.resource.ResourcePackLoader;
 import snownee.kiwi.Kiwi;
 import snownee.kiwi.KiwiTabBuilder;
 import snownee.kiwi.datagen.GameObjectLookup;
@@ -85,6 +111,8 @@ public class CommonProxy {
 					behaviorRegistry.setContext(null);
 				}
 			});
+
+			initLoader();
 		});
 		modEventBus.addListener((NewRegistryEvent event) -> {
 			ResourceLocation registryKey = new ResourceLocation(Kiwi.ID, "block_component");
@@ -97,6 +125,7 @@ public class CommonProxy {
 						Kiwi.registerRegistry($, KBlockComponent.Type.class);
 					});
 		});
+		modEventBus.addListener(XKDecoObjects::addMimicWallsToTab);
 
 		if (FMLEnvironment.dist.isClient()) {
 			ClientProxy.init();
@@ -122,7 +151,19 @@ public class CommonProxy {
 		});
 	}
 
-	public static void initLoader(ResourceManager resourceManager) {
+	public static void initLoader() {
+		Path packDirectory = FMLPaths.GAMEDIR.get().resolve("kiwipacks");
+		//noinspection ResultOfMethodCallIgnored
+		packDirectory.toFile().mkdirs();
+		FolderRepositorySource folderRepositorySource = new FolderRepositorySource(
+				packDirectory,
+				PackType.CLIENT_RESOURCES,
+				PackSource.DEFAULT);
+		PackRepository packRepository = new PackRepository(folderRepositorySource);
+		ResourcePackLoader.loadResourcePacks(packRepository, CommonProxy::buildPackFinder);
+		packRepository.reload();
+		packRepository.setSelected(packRepository.getAvailableIds());
+		ResourceManager resourceManager = new MultiPackResourceManager(PackType.CLIENT_RESOURCES, packRepository.openAllSelected());
 		var materials = JsonLoader.load(resourceManager, "kiwi/material", KMaterial.CODEC);
 		var tabs = JsonLoader.load(resourceManager, "kiwi/creative_tab", KCreativeTab.CODEC);
 		tabs.entrySet().stream().sorted(Comparator.comparingInt($ -> $.getValue()
@@ -141,7 +182,45 @@ public class CommonProxy {
 					.build();
 			Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, entry.getKey(), tab);
 		});
-		CreativeModeTabRegistry.sortTabs();
+	}
+
+	private static RepositorySource buildPackFinder(Map<IModFile, ? extends PathPackResources> modResourcePacks) {
+		return packAcceptor -> clientPackFinder(modResourcePacks, packAcceptor);
+	}
+
+	private static void clientPackFinder(Map<IModFile, ? extends PathPackResources> modResourcePacks, Consumer<Pack> packAcceptor) {
+		var hiddenPacks = new ArrayList<PathPackResources>();
+		for (Map.Entry<IModFile, ? extends PathPackResources> e : modResourcePacks.entrySet()) {
+			IModInfo mod = e.getKey().getModInfos().get(0);
+			final String name = "mod:" + mod.getModId();
+			final Pack modPack = Pack.readMetaAndCreate(
+					name,
+					Component.literal(e.getValue().packId()),
+					false,
+					id -> e.getValue(),
+					PackType.CLIENT_RESOURCES,
+					Pack.Position.BOTTOM,
+					PackSource.DEFAULT);
+			if (modPack == null) {
+				// Vanilla only logs an error, instead of propagating, so handle null and warn that something went wrong
+				ModLoader.get().addWarning(new ModLoadingWarning(mod, ModLoadingStage.ERROR, "fml.modloading.brokenresources", e.getKey()));
+				continue;
+			}
+			XKDeco.LOGGER.debug("Generating PackInfo named {} for mod file {}", name, e.getKey().getFilePath());
+			if (mod.getOwningFile().showAsResourcePack()) {
+				packAcceptor.accept(modPack);
+			} else {
+				hiddenPacks.add(e.getValue());
+			}
+		}
+
+		// Create a resource pack merging all mod resources that should be hidden
+		final Pack modResourcesPack = Pack.readMetaAndCreate("mod_resources", Component.literal("Mod Resources"), true,
+				id -> new DelegatingPackResources(id, false, new PackMetadataSection(
+						Component.translatable("fml.resources.modresources", hiddenPacks.size()),
+						SharedConstants.getCurrentVersion().getPackVersion(PackType.CLIENT_RESOURCES)), hiddenPacks),
+				PackType.CLIENT_RESOURCES, Pack.Position.BOTTOM, PackSource.DEFAULT);
+		packAcceptor.accept(modResourcesPack);
 	}
 
 	public static boolean isColorlessGlass(BlockState blockState) {
