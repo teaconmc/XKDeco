67075121c84c736d9648e38979a4fd73eba865cb
Snownee <i@snownee.com>
2026-01-09 01:14:58 +0800

    fix: block names
    (cherry picked from commit 47674c74b08108ca2138c4407a0a1d7dec00ab49)
    
    # Conflicts:
    #	src/generated/resources/.cache/33989c0035b4205b1c756b1db58662b0c55a3844
    #	src/main/java/org/teacon/xkdeco/data/XKDDataGen.java

--- src/main/java/org/teacon/xkdeco/data/XKDDataGen.java
+++ src/main/java/org/teacon/xkdeco/data/XKDDataGen.java
@@ -1,13 +1,21 @@
 package org.teacon.xkdeco.data;
 
+import java.util.Map;
+
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import snownee.kiwi.datagen.KiwiLanguageProvider;
 
 public class XKDDataGen implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator generator) {
 		FabricDataGenerator.Pack pack = generator.createPack();
-		pack.addProvider(XKDLanguageProvider::new);
+		pack.addProvider((output, registryLookup) -> new KiwiLanguageProvider(output) {
+			@Override
+			protected void generateGameObjectsEntries(Map<String, String> translationEntries) {
+				// NO-OP
+			}
+		});
 		pack.addProvider(XKDItemTagsProvider::new);
 		pack.addProvider(XKDBlockTagsProvider::new);
 		pack.addProvider(XKDModelProvider::new);
