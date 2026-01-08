3818e65868252793641a0bbc7d66c10ea39596a0
Snownee <i@snownee.com>
2026-01-09 05:00:47 +0800

    fix: roof ridge connection and roof ridge end models

---
+++ src/main/java/org/teacon/xkdeco/mixin/WallBlockMixin.java
@@ -0,0 +1,82 @@
+package org.teacon.xkdeco.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.teacon.xkdeco.block.RoofRidgeBlock;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.WallBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.WallSide;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import snownee.kiwi.customization.placement.PlaceSlot;
+
+@Mixin(WallBlock.class)
+public abstract class WallBlockMixin {
+	@Unique
+	private boolean xkdeco$isRoofRidge() {
+		return ((Object) this).getClass() == RoofRidgeBlock.class;
+	}
+
+	@Inject(method = "updateSides", at = @At("HEAD"), cancellable = true)
+	private void xkdeco$updateSides(
+			BlockState blockState,
+			boolean northConnection,
+			boolean eastConnection,
+			boolean southConnection,
+			boolean westConnection,
+			VoxelShape wallShape,
+			CallbackInfoReturnable<BlockState> cir) {
+		if (xkdeco$isRoofRidge()) {
+			cir.setReturnValue(blockState);
+		}
+	}
+
+	@Inject(method = "connectsTo", at = @At("HEAD"), cancellable = true)
+	private void xkdeco$connectsTo(BlockState blockState, boolean sideSolid, Direction direction, CallbackInfoReturnable<Boolean> cir) {
+		if (xkdeco$isRoofRidge()) {
+			cir.setReturnValue(
+					blockState.getBlock().getClass() == RoofRidgeBlock.class || !Block.isExceptionForConnection(blockState) && sideSolid ||
+							PlaceSlot.find(blockState, direction, "*roof_ridge_end").isPresent());
+		}
+	}
+
+	@Inject(method = "sideUpdate", at = @At("HEAD"), cancellable = true)
+	private void xkdeco$sideUpdate(
+			LevelReader level,
+			BlockPos firstPos,
+			BlockState firstState,
+			BlockPos secondPos,
+			BlockState secondState,
+			Direction direction,
+			CallbackInfoReturnable<BlockState> cir) {
+		if (xkdeco$isRoofRidge()) {
+			WallSide wallSide = RoofRidgeBlock.makeSide(level, secondPos, secondState, direction);
+			if (wallSide == null) {
+				wallSide = WallSide.LOW;
+			}
+			firstState = firstState.setValue(RoofRidgeBlock.DIRECTION_TO_PROPERTY.get(direction), wallSide);
+			firstState = firstState.setValue(WallBlock.UP, xkdeco$shouldRaisePost(firstState));
+			cir.setReturnValue(firstState);
+		}
+	}
+
+	@Unique
+	private boolean xkdeco$shouldRaisePost(BlockState state) {
+		WallSide wallside = state.getValue(WallBlock.NORTH_WALL);
+		WallSide wallside1 = state.getValue(WallBlock.SOUTH_WALL);
+		WallSide wallside2 = state.getValue(WallBlock.EAST_WALL);
+		WallSide wallside3 = state.getValue(WallBlock.WEST_WALL);
+		boolean flag1 = wallside1 == WallSide.NONE;
+		boolean flag2 = wallside3 == WallSide.NONE;
+		boolean flag3 = wallside2 == WallSide.NONE;
+		boolean flag4 = wallside == WallSide.NONE;
+		return flag4 && flag1 && flag2 && flag3 || flag4 != flag1 || flag2 != flag3;
+	}
+}
